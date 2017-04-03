c716074ce189f07015b328c8c620885dca2b2abd
Nick Monto <monto.nicholas@gmail.com>
2017-04-03 12:29:52 -0400

    Removed Savefigure function
    Using the pylab built in save function

--- create_spectrograms.py
+++ create_spectrograms.py
@@ -16,36 +16,6 @@ for l in languages:
     audio_dict[l] = sorted(os.listdir(INPUT_FOLDER + l))
 
 # constructing spectrogram loop
-
-def SaveFigureAsImage(fileName,fig=None,**kwargs):
-    ''' Save a Matplotlib figure as an image without borders or frames.
-       Args:
-            fileName (str): String that ends in .png etc.
-
-            fig (Matplotlib figure instance): figure you want to save as the image
-        Keyword Args:
-            orig_size (tuple): width, height of the original image used to maintain
-            aspect ratio.
-
-        From: http://robotics.usc.edu/~ampereir/wordpress/?p=626
-    '''
-    fig_size = fig.get_size_inches()
-    w,h = fig_size[0], fig_size[1]
-    fig.patch.set_alpha(0)
-    if kwargs.has_key('orig_size'):  # Aspect ratio scaling if required
-        w, h = kwargs['orig_size']
-        w2, h2 = fig_size[0], fig_size[1]
-        fig.set_size_inches([(w2/w)*w, (w2/w)*h])
-        fig.set_dpi((w2/w)*fig.get_dpi())
-    a = fig.gca()
-    a.set_frame_on(False)
-    a.set_xticks([]); a.set_yticks([])
-    plt.axis('off')
-    plt.xlim(0, h); plt.ylim(w,0)
-    fig.savefig(fileName, transparent=True, bbox_inches='tight',
-                pad_inches=0)
-
-
 def plot_spectrogram(audiopath, binsize=2**10, plotpath=None, ylim=(0, 5500)):
     fs, data = wavfile.read(audiopath)
 
